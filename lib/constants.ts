ce8fbfe1157b5ea83a61a5970d1bc39ffe805f50
jose <jmourinho14@gmail.com>
2025-10-05 19:28:33 -0700

    Implement stock details page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,6 @@
 export const NAV_ITEMS = [
     { href: '/', label: 'Dashboard' },
-    { href: '/search', label: 'Search' },
-    { href: '/watchlist', label: 'Watchlist' },
+    { href: '/search', label: 'Search' }
     // { href: '/watchlist', label: 'Watchlist' },
 ];
 
